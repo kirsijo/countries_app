50a7b8faa9e90e010a3c4bebfd481915728699fb
Kirsi Jokela <kirsijo@gmail.com>
2022-08-19 18:08:58 +0300

    overlay on homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,6 +4,7 @@ const Home = () => {
     return (
         <>
         <Nav/>
+        <div className="overlay-div"><h5>Country app is a project I made in React classes at Business College Helsinki.</h5></div>
         </>
     )
 }
