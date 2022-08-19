3d6ba806674110d0754dc8d3525e333c5d4d1711
Kirsi Jokela <kirsijo@gmail.com>
2022-08-19 18:27:36 +0300

    h5 and h6 font colours

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
     return (
         <>
         <Nav/>
-        <div className="overlay-div"><h5>Country app is a project I made in React classes at Business College Helsinki.</h5></div>
+        <div className="overlay-div"><h6>Country app is a project I made in React classes at Business College Helsinki.</h6></div>
         </>
     )
 }
